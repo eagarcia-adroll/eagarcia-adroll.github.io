e53e8fcc1fd17e15f1b18345c79fc97fcd29f283
Emmanuel Garcia <emmanuel.garcia@adroll.com>
2023-08-29 08:14:51 -0600

    .

--- known-customer.js
+++ known-customer.js
@@ -2,9 +2,8 @@ class KnownCustomerOperation {
     async run() {
         const knownCustomer = await sharedStorage.get('known-customer');
         console.log(knownCustomer);
-        console.log(document);
+        console.log(privateAggregation);
         console.log(navigator);
-
     }
 }
 
